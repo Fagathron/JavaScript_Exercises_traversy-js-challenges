836876ee551d667f4434f830167d6be3d8398d9e
Alex <faga@MacBook-Pro-Macbook.local>
2025-03-10 23:28:24 +0200

    Second commit: 01 and 02ish

--- 01-basic-challenges-1/04-count-occurrences/count-occurrences.test.js
+++ 01-basic-challenges-1/04-count-occurrences/count-occurrences.test.js
@@ -2,6 +2,6 @@ const countOccurrences = require('./count-occurrences');
 
 test('Count Occurrences of a Character', () => {
   expect(countOccurrences('hello', 'l')).toBe(2);
-  expect(countOccurrences('programming', 'm')).toBe(2);
+  expect(countOccurrences('programming', 'z')).toBe(0);
   expect(countOccurrences('banana', 'a')).toBe(3);
 });
